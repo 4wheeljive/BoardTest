f52b65db92c61437b291a364989d27f12c8d9f33
4wheeljive <135266120+4wheeljive@users.noreply.github.com>
2026-01-29 01:30:24 -0800

    pre-library rebuild

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <Arduino.h>
 #include <FastLED.h>
-#include "reference/matrixMap_32x48_3pin.h" 
+//#include "platforms/esp/32/drivers/rmt/rmt_5/rmt_memory_manager.h"
+//#include "esp_log.h"
+#include "reference/matrixMap_32x48_3pin.h"
+//#include "reference/matrixMap_22x22.h" 
 
 #define PIN0 2
 #define PIN1 3
@@ -9,8 +12,12 @@
 #define WIDTH 48
 #define NUM_STRIPS 3
 #define NUM_LEDS_PER_STRIP 512
+//#define HEIGHT 22 
+//#define WIDTH 22
+//#define NUM_STRIPS 1
+//#define NUM_LEDS_PER_STRIP 484
 
-using namespace fl;
+//using namespace fl;
 
 #define NUM_LEDS ( WIDTH * HEIGHT )
 const uint16_t MIN_DIMENSION = min(WIDTH, HEIGHT);
@@ -32,15 +39,26 @@ uint16_t myXY(uint8_t x, uint8_t y) {
   return ledNum;
 }	
 
-
 //************************************************************************************************************
 
 void setup() {
 
+   // Suppress ESP-IDF cache sync errors early (before any FastLED init)
+   //esp_log_level_set("cache", ESP_LOG_NONE);
+   //esp_log_level_set("rmt", ESP_LOG_NONE);
+
    Serial.begin(115200);
    delay(1000);
 
-   //FastLED.setExclusiveDriver("RMT");
+   // Reserve external RMT memory usage (USB CDC or other drivers)
+   //auto& rmtMgr = fl::RmtMemoryManager::instance();
+   //rmtMgr.reserveExternalMemory(48, 0);
+
+   FastLED.setDriverEnabled("SPI", false); 
+   FastLED.setDriverEnabled("UART", false);
+   FastLED.setDriverEnabled("I2S", false);
+   FastLED.setDriverEnabled("I2S LCD_CAM", false);
+   FastLED.setExclusiveDriver("RMT");
 
    FastLED.addLeds<WS2812B, PIN0, GRB>(leds, 0, NUM_LEDS_PER_STRIP)
      .setCorrection(TypicalLEDStrip);
@@ -61,10 +79,14 @@ void setup() {
 //************************************************************************************************************
 
 void loop() {
-
-  EVERY_N_SECONDS(2){
-    Serial.println("Loop...");
+  
+  /*
+  EVERY_N_SECONDS(3) {
+    //FASTLED_DBG("Loop...");
+    uint8_t fps = FastLED.getFPS();
+    FASTLED_DBG(fps << " fps");
   }
+  */
 
   static uint8_t hue = 0;
   
